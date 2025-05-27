893860180dc8544cfc3e0ceac7c565255a27497c
ben <86694445+0xbdg@users.noreply.github.com>
2025-05-27 21:49:06 +0700

    Add files via upload

---
+++ lua/config/options.lua
@@ -0,0 +1,22 @@
+vim.wo.number=true
+vim.o.relativenumber=true
+vim.o.clipboard='unnamedplus'
+vim.o.wrap=true
+vim.o.linebreak=true
+vim.o.mouse='a'
+vim.o.autoindent=true
+vim.o.showmatch = true        
+vim.o.foldmethod = 'marker'   
+vim.o.colorcolumn = '80'      
+vim.o.splitright = true   
+vim.o.splitbelow = true       
+vim.o.ignorecase = true       
+vim.o.smartcase = true        
+vim.o.termguicolors = true    
+vim.o.laststatus=3            
+vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.o.expandtab = true        -- Use spaces instead of tabs
+vim.o.shiftwidth = 4          -- Shift 4 spaces when tab
+vim.o.tabstop = 4             -- 1 tab == 4 spaces
+vim.o.smartindent = true      -- Autoindent new lines
+
