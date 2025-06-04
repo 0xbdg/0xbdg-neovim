6f140093823011a0b4a8abdbf7e4c664ce83689a
?0 <benjamindguntara@protonmail.com>
2025-06-04 10:39:35 +0000

    dksalkdjalkj

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -1,48 +1,32 @@
+
+-- Custom Parameters (with defaults)
 return {
-  "nomnivore/ollama.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
-  -- All the user commands added by the plugin
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-  keys = {
-    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>o",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
-      mode = { "n", "v" },
-    },
-
-    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>g",
-      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-      desc = "ollama Generate Code",
-      mode = { "n", "v" },
-    },
-  },
-
-  ---@type Ollama.Config
-  opts = {
-    model = "deepseek",
-    url = "http://IP:11434",
-    serve = {
-        on_start = false,
-        command = "ollama",
-        args = { "serve" },
-        stop_command = "pkill",
-        stop_args = { "-SIGTERM", "ollama" },
-    },
-    prompts = {
-      Sample_Prompt = {
-        prompt = "Hi there",
-        input_label = "> ",
-        model = "deepseek",
-        action = "display",
-      }
+    "David-Kunz/gen.nvim",
+    opts = {
+        model = "tinyllama:latest", -- The default model to use.
+        quit_map = "q", -- set keymap to close the response window
+        retry_map = "<c-r>", -- set keymap to re-send the current prompt
+        accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
+        host = "localhost", -- The host running the Ollama service.
+        port = "11434", -- The port on which the Ollama service is listening.
+        display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
+        show_prompt = true, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+        show_model = true, -- Displays which model you are using at the beginning of your chat session.
+        no_auto_close = false, -- Never closes the window automatically.
+        file = false, -- Write the payload to a temporary file to keep the command short.
+        hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+        init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+        -- Function to initialize Ollama
+        command = function(options)
+            local body = {model = options.model, stream = true}
+            return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/generate -d $body"
+        end,
+        -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+        -- This can also be a command string.
+        -- The executed command must return a JSON object with { response, context }
+        -- (context property is optional).
+        -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+        result_filetype = "markdown", -- Configure filetype of the result buffer
+        debug = false -- Prints errors and the command which is run.
     }
-  }
 }
