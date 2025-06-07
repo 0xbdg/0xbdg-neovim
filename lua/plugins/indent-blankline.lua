2df8e443866e947fcda1b8caf4e885e8c985fefc
0xbdg <benjamindguntara@protonmail.com>
2025-06-07 16:30:14 +0000

    dasojdsaidjas

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,11 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPost",
     main = "ibl",
     ---@module "ibl"
     ---@type ibl.config
         
-    opts = {},
+    opts = {
+        enabled = true    
+    },
 }
