ae8999d130012127dfef5c4acb81f4f84f817065
0xbdg <benjamindguntara@protonmail.com>
2025-07-20 22:21:56 +0700

    dsapsdpsokd

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,6 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "lua_ls", "pyright", "arduino_language_server", "rust_analyzer", "intelephense", "clangd"},},
+    opts = { ensure_installed = { "lua_ls", "pyright", "arduino_language_server", "rust_analyzer", "intelephense", "clangd", "gopls"},},
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
