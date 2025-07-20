40e7c197650a19a12146bcec88b1087969f59787
0xbdg <benjamindguntara@protonmail.com>
2025-07-20 22:01:56 +0700

    sdaj

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,6 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "lua_ls", "pyright", "arduino_language_server", "rust_analyzer", "intelephense"},},
+    opts = { ensure_installed = { "lua_ls", "pyright", "arduino_language_server", "rust_analyzer", "intelephense", "clangd"},},
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
