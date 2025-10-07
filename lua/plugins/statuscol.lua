9bdabfa1083f1614552e08a4b92214662a09272a
?0 <benjamindguntara@protonmail.com>
2025-10-07 14:01:17 +0000

    disajiasjd

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,60 +1,78 @@
 return {
-  "luukvbaal/statuscol.nvim", config = function()
+  "luukvbaal/statuscol.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
-        setopt = true,         -- Whether to set the 'statuscolumn' option, may be set to false for those who
-                         -- want to use the click handlers in their own 'statuscolumn': _G.Sc[SFL]a().
-                         -- Although I recommend just using the segments field below to build your
-                         -- statuscolumn to benefit from the performance optimizations in this plugin.
-  -- builtin.lnumfunc number string options
-        thousands = false,     -- or line number thousands separator string ("." / ",")
-        relculright = true,   -- whether to right-align the cursor line number with 'relativenumber' set
-  -- Builtin 'statuscolumn' options
-        ft_ignore = nil,       -- Lua table with 'filetype' values for which 'statuscolumn' will be unset
-        bt_ignore = nil,       -- Lua table with 'buftype' values for which 'statuscolumn' will be unset
+      setopt = true,
+      relculright = true,
+      thousands = false,
 
-        segments = {
-            { text = { "%C" }, click = "v:lua.ScFa" },
-            { text = { "%s" }, click = "v:lua.ScSa" },
-            {
-            text = { builtin.lnumfunc, " " },
-            condition = { true, builtin.not_empty },
-            click = "v:lua.ScLa",
-            }
+      ft_ignore = {
+        "NvimTree", "neo-tree", "Trouble", "lazy", "mason", "qf", "help"
+      },
+      bt_ignore = {
+        "terminal", "nofile", "neo-tree"
+      },
+
+      segments = {
+        {
+          text = { builtin.foldfunc, " " },
+          click = "v:lua.ScFa",
+          hl = "FoldColumn",
+          condition = { true, builtin.not_empty },
         },
 
-        clickmod = "c",         -- modifier used for certain actions in the builtin clickhandlers:
-                          -- "a" for Alt, "c" for Ctrl and "m" for Meta.
-        clickhandlers = {       -- builtin click handlers, keys are pattern matched
-            Lnum                    = builtin.lnum_click,
-            FoldClose               = builtin.foldclose_click,
-            FoldOpen                = builtin.foldopen_click,
-            FoldOther               = builtin.foldother_click,
-            DapBreakpointRejected   = builtin.toggle_breakpoint,
-            DapBreakpoint           = builtin.toggle_breakpoint,
-            DapBreakpointCondition  = builtin.toggle_breakpoint,
-            ["diagnostic/signs"]    = builtin.diagnostic_click,
-            gitsigns                = builtin.gitsigns_click,
+        {
+            sign = {
+                namespace = { "diagnostic/signs" },
+                maxwidth = 1,
+                auto = true,
+            },
+            click = "v:lua.ScSa",
         },
-        lnum = 43,             -- v:lnum
-        relnum = 5,            -- v:relnum
-        virtnum = 0,           -- v:virtnum
-        buf = 1,               -- buffer handle of drawn window
-        win = 1000,            -- window handle of drawn window
-        actual_curbuf = 1,     -- buffer handle of |g:actual_curwin|
-        actual_curwin = 1000,  -- window handle of |g:actual_curbuf|
-        nu = true,             -- 'number' option value
-        rnu = true,            -- 'relativenumber' option value
-        empty = true,          -- statuscolumn is currently empty
-        fold = {
-            width = 1,           -- current width of the fold column
-    -- ' fillchars' option values:
-            close = "",         -- foldclose
-            open = "",          -- foldopen
-            sep = " "            -- foldsep
+
+        {
+          sign = {
+            name = { "gitsigns" },
+            maxwidth = 2,
+            auto = true,
+            colwidth = 1,
+            wrap=false,
+            foldclosed = true,
+            fillchar = " ",
+          },
+          click = "v:lua.ScSa",
         },
+       
     
+       
+      },
 
+      clickmod = "c",
+      clickhandlers = {
+        Lnum = builtin.lnum_click,
+        FoldClose = builtin.foldclose_click,
+        FoldOpen = builtin.foldopen_click,
+        FoldOther = builtin.foldother_click,
+        ["diagnostic/signs"] = builtin.diagnostic_click,
+        gitsigns = builtin.gitsigns_click,
+      },
     })
+
+    -- pengaturan fold supaya tampilannya baik
+    vim.o.foldenable = true
+    vim.o.foldmethod = "indent"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldcolumn = "1"
+    /*
+    vim.opt.fillchars:append({
+      fold = " ",
+      foldopen = "",
+      foldclose = "",
+      foldsep = " ",
+      eob = " ",
+    })*/
   end,
 }
